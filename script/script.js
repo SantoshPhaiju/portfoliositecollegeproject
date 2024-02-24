de31e0299209f5803474e3bfc826e6b098ab57ba
Santosh Phaiju <santoshphaiju062@gmail.com>
2024-02-24 16:06:38 +0545

    made 50% reponsive

--- script/script.js
+++ script/script.js
@@ -1,16 +1,22 @@
 var swiper = new Swiper(".mySwiper", {
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 30,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
   },
 });
